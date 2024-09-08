96fb8ef38334493b0e9b09139818cb05ce97e3ee
prof85 <elicardalfer.cursos@gmail.com>
2024-09-08 01:21:39 -0300

    Update formato_texto.py

--- tema3-8b_des3/formato_texto.py
+++ tema3-8b_des3/formato_texto.py
@@ -1,23 +1,49 @@
 # formato_texto.py
 
 # NEGRITA ----
+'''
+Texto negrita en códigos ANSI.
+Args:
+        texto (str): El texto a formatear.
+Returns:
+        str: Texto en negrita.
+'''
 
 def negrita(texto):
         return f'\033[1m{texto}\033[0m'
 
 # CURSIVA ----
-
+'''
+Texto cursiva en códigos ANSI.
+Args:
+        texto (str): El texto a formatear.
+Returns:
+        str: Texto en cursiva.
+'''
 def cursiva(texto):
         return f'\033[3m{texto}\033[0m'
 
 # SUBRAYADO ----
-
+'''
+Texto subrayado en códigos ANSI.
+Args:
+        texto (str): El texto a formatear.
+Returns:
+        str: Texto subrayado
+'''
 def subrayado(texto):
         return f'\033[4m{texto}\033[0m'
 
 # COLOR TEXTO----
 # Colores: rojo, verde, amarillo, azul, magenta, cian, blanco.
-
+'''
+Devuelve el texto con el color especificado utilizando códigos ANSI.
+Args:
+        texto (str): El texto a colorear.
+        nombre_color (str): El nombre del color del texto (rojo, verde, amarillo, azul, magenta, cian, blanco).
+Returns:
+        str: Texto coloreado.
+'''
 def color(texto, nombre_color):
         colores = {
             'rojo': '\033[31m',
@@ -28,11 +54,20 @@ def color(texto, nombre_color):
             'cian': '\033[36m',
             'blanco': '\033[37m'
         }
+         # Obtiene el código de color de texto, usa blanco por defecto si el color no es válido
         codigo_color = colores.get(nombre_color.lower(), '\033[37m')
         return f'{codigo_color}{texto}\033[0m'
 
 # COLOR FONDO
 # Colores: rojo, verde, amarillo, azul, magenta, cian, blanco.
+'''
+Devuelve el texto con el color de fondo especificado utilizando códigos ANSI.
+Args:
+        texto (str): El texto a formatear.
+        nombre_colorf (str): El nombre del color de fondo (rojo, verde, amarillo, azul, magenta, cian, blanco).
+Returns:
+        str: Texto con el color de fondo aplicado.
+'''
 def colorf(texto, nombre_colorf):
         colores_fondo = {
             'rojo': '\033[41m',
@@ -43,16 +78,29 @@ def colorf(texto, nombre_colorf):
             'cian': '\033[46m',
             'blanco': '\033[47m'
         }
+         # Obtiene el código de color de fondo, usa blanco por defecto si el color no es válido
         codigo_colorf = colores_fondo.get(nombre_colorf.lower(), '\033[0m')
         return f'{codigo_colorf}{texto}\033[0m'
 
 # MAYÚSCULAS ----
-
+'''
+Convierte el texto a mayúsculas.
+Args:
+        texto (str): El texto a convertir.
+Returns:
+        str: Texto en mayúsculas.
+'''
 def a_mayusculas(texto):
         return texto.upper()
 
 # MINÚSCULAS ----
-
+'''
+Convierte el texto a minúsculas.
+Args:
+        texto (str): El texto a convertir.
+Returns:
+        str: Texto en minúsculas.
+'''
 def a_minusculas(texto):
         return texto.lower()
 
